bb3576c3966288a621a0c15fa7e960ad4c33db78
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2014-10-20 17:45:24 +0100

    Restructured resizer inner loop
    Was *very* slow for large up and down ratios, e.g. as used for
    conversion from 13.5MHz to 4fsc sampling.

--- src/pyctools/components/interp/resizecore.pyx
+++ src/pyctools/components/interp/resizecore.pyx
@@ -30,96 +30,53 @@ ctypedef numpy.float32_t DTYPE_t
 def resize_line1(numpy.ndarray[DTYPE_t, ndim=1] out_line,
                  numpy.ndarray[DTYPE_t, ndim=1] in_line,
                  numpy.ndarray[DTYPE_t, ndim=1] norm_filter,
-                 unsigned int x_up, unsigned int x_down):
+                 int x_up, int x_down):
     cdef:
-        unsigned int xlen_in, xlen_out, xlen_fil
-        unsigned int d_fil
-        unsigned int x_in_phase, x_out_phase, x_in_0, x_out_0
-        unsigned int x_in, x_out, x_fil
-        int x_fil_0
-        DTYPE_t coef
+        int xlen_in, xlen_out, xlen_fil
+        int x_in, x_out, x_fil, x_fil_off
+        DTYPE_t acc
     xlen_in = in_line.shape[0]
     xlen_out = out_line.shape[0]
     xlen_fil = norm_filter.shape[0]
-    d_fil = x_up * x_down
-    for x_in_phase in range(x_down):
-        for x_out_phase in range(x_up):
-            x_in_0 = x_in_phase
-            x_out_0 = x_out_phase
-            x_fil_0 = (x_out_0 * x_down) - (x_in_0 * x_up)
-            # add offset as filter aperture is symmetrical
-            x_fil_0 += (xlen_fil - 1) // 2
-            # find lowest coefficient for this phase
-            while x_fil_0 >= d_fil:
-                x_fil_0 -= d_fil
-                x_in_0 += x_down
-            while x_fil_0 < 0:
-                x_fil_0 += d_fil
-                x_out_0 += x_up
-            # iterate over all coefficients for this phase
-            x_fil = x_fil_0
-            while x_fil < xlen_fil:
-                coef = norm_filter[x_fil]
-                if coef != 0.0:
-                    x_in = x_in_0
-                    x_out = x_out_0
-                    while x_in < xlen_in and x_out < xlen_out:
-                        out_line[x_out] += in_line[x_in] * coef
-                        x_in += x_down
-                        x_out += x_up
-                x_fil += d_fil
-                # increment x_in_0 or x_out_0 to match increment in x_fil
-                if x_in_0 >= x_down:
-                    x_in_0 -= x_down
-                else:
-                    x_out_0 += x_up
+    # offset as filter is symmetrical
+    x_fil_off = (xlen_fil - 1) // 2
+    for x_out in range(xlen_out):
+        x_fil = (xlen_fil - 1) - x_fil_off
+        x_in = max(((x_out * x_down) + (x_up - 1) - x_fil) // x_up, 0)
+        x_fil = ((x_out * x_down) - (x_in * x_up)) + x_fil_off
+        acc = out_line[<unsigned int>x_out]
+        while x_fil >= 0 and x_in < xlen_in:
+            acc += in_line[<unsigned int>x_in] * norm_filter[<unsigned int>x_fil]
+            x_fil -= x_up
+            x_in += 1
+        out_line[x_out] = acc
 
 @cython.boundscheck(False)
 def resize_line2(numpy.ndarray[DTYPE_t, ndim=2] out_line,
                  numpy.ndarray[DTYPE_t, ndim=2] in_line,
                  numpy.ndarray[DTYPE_t, ndim=1] norm_filter,
-                 unsigned int x_up, unsigned int x_down):
+                 int x_up, int x_down):
     cdef:
-        unsigned int xlen_in, xlen_out, xlen_fil
-        unsigned int comps, d_fil
-        unsigned int x_in_phase, x_out_phase, x_in_0, x_out_0
-        unsigned int x_in, x_out, x_fil
-        int x_fil_0
-        DTYPE_t coef
+        int xlen_in, xlen_out, xlen_fil
+        int x_in, x_out, x_fil, x_fil_off, x_in_0, x_fil_0
+        unsigned int comps, c
+        DTYPE_t acc
     xlen_in = in_line.shape[0]
     xlen_out = out_line.shape[0]
     xlen_fil = norm_filter.shape[0]
     comps = in_line.shape[1]
-    d_fil = x_up * x_down
-    for x_in_phase in range(x_down):
-        for x_out_phase in range(x_up):
-            x_in_0 = x_in_phase
-            x_out_0 = x_out_phase
-            x_fil_0 = (x_out_0 * x_down) - (x_in_0 * x_up)
-            # add offset as filter aperture is symmetrical
-            x_fil_0 += (xlen_fil - 1) // 2
-            # find lowest coefficient for this phase
-            while x_fil_0 >= d_fil:
-                x_fil_0 -= d_fil
-                x_in_0 += x_down
-            while x_fil_0 < 0:
-                x_fil_0 += d_fil
-                x_out_0 += x_up
-            # iterate over all coefficients for this phase
+    # offset as filter is symmetrical
+    x_fil_off = (xlen_fil - 1) // 2
+    for x_out in range(xlen_out):
+        x_fil = (xlen_fil - 1) - x_fil_off
+        x_in_0 = max(((x_out * x_down) + (x_up - 1) - x_fil) // x_up, 0)
+        x_fil_0 = ((x_out * x_down) - (x_in_0 * x_up)) + x_fil_off
+        for c in range(comps):
+            x_in = x_in_0
             x_fil = x_fil_0
-            while x_fil < xlen_fil:
-                coef = norm_filter[x_fil]
-                if coef != 0.0:
-                    for c in range(comps):
-                        x_in = x_in_0
-                        x_out = x_out_0
-                        while x_in < xlen_in and x_out < xlen_out:
-                            out_line[x_out, c] += in_line[x_in, c] * coef
-                            x_in += x_down
-                            x_out += x_up
-                x_fil += d_fil
-                # increment x_in_0 or x_out_0 to match increment in x_fil
-                if x_in_0 >= x_down:
-                    x_in_0 -= x_down
-                else:
-                    x_out_0 += x_up
+            acc = out_line[<unsigned int>x_out, c]
+            while x_fil >= 0 and x_in < xlen_in:
+                acc += in_line[<unsigned int>x_in, c] * norm_filter[<unsigned int>x_fil]
+                x_fil -= x_up
+                x_in += 1
+            out_line[x_out, c] = acc
