d563db2fc969304e310b433f0db7fe594be97eec
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2014-11-10 13:42:26 +0000

    Use a generator to read file

--- src/pyctools/components/io/videofilereader.py
+++ src/pyctools/components/io/videofilereader.py
@@ -39,6 +39,7 @@ from __future__ import print_function
 __all__ = ['VideoFileReader']
 __docformat__ = 'restructuredtext en'
 
+from contextlib import contextmanager
 import logging
 import os
 import re
@@ -55,105 +56,98 @@ class VideoFileReader(Component):
     with_outframe_pool = True
 
     def initialise(self):
-        self.ffmpeg = None
+        self.frame_no = 0
+        self.generator = None
         self.config['path'] = ConfigPath()
         self.config['looping'] = ConfigEnum(('off', 'repeat'), dynamic=True)
         self.config['type'] = ConfigEnum(('RGB', 'Y'))
         self.config['16bit'] = ConfigEnum(('off', 'on'))
 
-    def process_start(self):
-        super(VideoFileReader, self).process_start()
+    @contextmanager
+    def subprocess(self, *arg, **kw):
+        try:
+            sp = subprocess.Popen(*arg, **kw)
+            yield sp
+        finally:
+            sp.terminate()
+            sp.stdout.close()
+            sp.stderr.close()
+            sp.wait()
+
+    def file_reader(self):
+        """Generator process to read file"""
         self.update_config()
         path = self.config['path']
-        # open file to get dimensions
-        self.ffmpeg = subprocess.Popen(
-            ['ffmpeg', '-v', 'info', '-y', '-an', '-vn', '-i', path, '-'],
-            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-            universal_newlines=True)
-        for line in self.ffmpeg.stderr.read().splitlines():
-            match = re.search('(\d{2,})x(\d{2,})', line)
-            if match:
-                self.xlen, self.ylen = map(int, match.groups())
-                break
-        else:
-            self.logger.critical('Failed to open %s', path)
-            self.output(None)
-            self.stop()
-            return
-        self.ffmpeg.stderr.flush()
-        self.open_file()
         self.metadata = Metadata().from_file(path)
         audit = self.metadata.get('audit')
         audit += 'data = %s\n' % path
         audit += '    type: %s, 16bit: %s\n' % (
             self.config['type'], self.config['16bit'])
         self.metadata.set('audit', audit)
-        self.frame_no = 0
-
-    def close_file(self):
-        if self.ffmpeg:
-            self.ffmpeg.terminate()
-            self.ffmpeg.stdout.flush()
-            self.ffmpeg = None
-
-    def open_file(self):
-        self.close_file()
-        path = self.config['path']
-        self.bit16 = self.config['16bit'] != 'off'
-        self.frame_type = self.config['type']
-        if self.frame_type == 'RGB':
-            self.bps = 3
-            if self.bit16:
-                pix_fmt = 'rgb48le'
+        # open file to get dimensions
+        with self.subprocess(
+                ['ffmpeg', '-v', 'info', '-y', '-an', '-vn', '-i', path, '-'],
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                universal_newlines=True) as sp:
+            for line in sp.stderr.read().splitlines():
+                match = re.search('(\d{2,})x(\d{2,})', line)
+                if match:
+                    xlen, ylen = map(int, match.groups())
+                    break
             else:
-                pix_fmt = 'rgb24'
-        else:
-            self.bps = 1
-            if self.bit16:
-                pix_fmt = 'gray16le'
+                self.logger.critical('Failed to open %s', path)
+                return
+        while True:
+            bit16 = self.config['16bit'] != 'off'
+            self.frame_type = self.config['type']
+            if self.frame_type == 'RGB':
+                bps = 3
+                pix_fmt = ('rgb24', 'rgb48le')[bit16]
             else:
-                pix_fmt = 'gray'
-        self.bytes_per_line = self.xlen * self.ylen * self.bps
-        if self.bit16:
-            self.bytes_per_line *= 2
-        # open file to read data
-        self.ffmpeg = subprocess.Popen(
-            ['ffmpeg', '-v', 'warning', '-an', '-i', path,
-             '-f', 'image2pipe', '-pix_fmt', pix_fmt,
-             '-c:v', 'rawvideo', '-'],
-            stdout=subprocess.PIPE, bufsize=self.bytes_per_line)
+                bps = 1
+                pix_fmt = ('gray', 'gray16le')[bit16]
+            bytes_per_line = xlen * ylen * bps
+            if bit16:
+                bytes_per_line *= 2
+            # open file to read data
+            with self.subprocess(
+                    ['ffmpeg', '-v', 'warning', '-an', '-i', path,
+                     '-f', 'image2pipe', '-pix_fmt', pix_fmt,
+                     '-c:v', 'rawvideo', '-'],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                    bufsize=bytes_per_line) as sp:
+                while True:
+                    raw_data = sp.stdout.read(bytes_per_line)
+                    if len(raw_data) < bytes_per_line:
+                        break
+                    if bit16:
+                        image = numpy.fromstring(raw_data, dtype=numpy.uint16)
+                        image = image.astype(numpy.float32) / 256.0
+                    else:
+                        image = numpy.fromstring(raw_data, dtype=numpy.uint8)
+                    yield [image.reshape((ylen, xlen, bps))]
+            self.update_config()
+            if self.frame_no == 0 or self.config['looping'] == 'off':
+                return
 
     @actor_method
     def new_out_frame(self, frame):
         """new_out_frame(frame)
 
         """
-        self.update_config()
-        while True:
-            raw_data = self.ffmpeg.stdout.read(self.bytes_per_line)
-            if len(raw_data) >= self.bytes_per_line:
-                break
-            self.close_file()
-            if self.frame_no == 0 or self.config['looping'] == 'off':
-                self.output(None)
-                self.stop()
-                return
-            self.open_file()
-        if self.bit16:
-            image = numpy.fromstring(raw_data, dtype=numpy.uint16)
-            image = image.astype(numpy.float32) / 256.0
-        else:
-            image = numpy.fromstring(raw_data, dtype=numpy.uint8)
-        frame.data = [image.reshape((self.ylen, self.xlen, self.bps))]
+        if not self.generator:
+            self.generator = self.file_reader()
+        try:
+            frame.data = next(self.generator)
+        except StopIteration:
+            self.output(None)
+            self.stop()
+            return
         frame.type = self.frame_type
         frame.frame_no = self.frame_no
         self.frame_no += 1
         frame.metadata.copy(self.metadata)
         self.output(frame)
-
-    def onStop(self):
-        super(VideoFileReader, self).onStop()
-        self.close_file()
 
 def main():
     from PyQt4 import QtGui
