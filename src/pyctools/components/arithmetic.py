28bb07c1bd109ca8d89c37d5075a57147dcf0248
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-02-05 09:30:53 +0000

    Added a 2-input arithmetic component

--- src/pyctools/components/arithmetic.py
+++ src/pyctools/components/arithmetic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #  Pyctools - a picture processing algorithm development kit.
 #  http://github.com/jim-easterbrook/pyctools
-#  Copyright (C) 2014  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#  Copyright (C) 2014-15  Jim Easterbrook  jim@jim-easterbrook.me.uk
 #
 #  This program is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -38,15 +38,20 @@ Note the liberal use of ``pt_float`` to coerce data to the Pyctools
 default floating point type (``numpy.float32``). NumPy will otherwise
 convert Python :py:class:`float` to ``numpy.float64``.
 
+Arithmetic2 is similar, but has two inputs. For example, to subtract the
+second inout from the first you could do::
+
+    subtracter = Arithmetic2(func='data1 - data2')
+
 """
 
-__all__ = ['Arithmetic']
+__all__ = ['Arithmetic', 'Arithmetic2']
 __docformat__ = 'restructuredtext en'
 
 import numpy
 
 from pyctools.core.config import ConfigStr
-from pyctools.core.base import Transformer
+from pyctools.core.base import Component, Transformer
 from pyctools.core.types import pt_float, pt_complex
 
 class Arithmetic(Transformer):
@@ -62,3 +67,27 @@ class Arithmetic(Transformer):
         audit += 'data = %s\n' % func
         out_frame.metadata.set('audit', audit)
         return True
+
+
+class Arithmetic2(Component):
+    inputs = ['input1', 'input2']
+    with_outframe_pool = True
+
+    def initialise(self):
+        self.config['func'] = ConfigStr(value='data1 + data2')
+
+    def process_frame(self):
+        in_frame1 = self.input_buffer['input1'].get()
+        in_frame2 = self.input_buffer['input2'].get()
+        out_frame = self.outframe_pool['output'].get()
+        self.update_config()
+        func = self.config['func']
+        data1 = in_frame1.as_numpy()
+        data2 = in_frame2.as_numpy()
+        out_frame.initialise(in_frame1)
+        out_frame.data = eval(func)
+        audit = 'data1 = {\n%s}\n' % in_frame1.metadata.get('audit')
+        audit += 'data2 = {\n%s}\n' % in_frame2.metadata.get('audit')
+        audit += 'data = %s\n' % func
+        out_frame.metadata.set('audit', audit)
+        self.output(out_frame)
