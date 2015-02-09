edb49d77fcefaece7e784d866e9bb1febcf6d4bc
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-02-09 09:51:42 +0000

    Added simple zero-inserting deinterlacer

---
+++ src/pyctools/components/deinterlace/insertzero.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+#  Pyctools - a picture processing algorithm development kit.
+#  http://github.com/jim-easterbrook/pyctools
+#  Copyright (C) 2015  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#
+#  This program is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see
+#  <http://www.gnu.org/licenses/>.
+
+"""Zero-inserting interlace to sequential converter.
+
+This simply rearranges each interlaced frame into two frames where half
+the lines in each are filled with zero. It can be used for viewing
+pictures directly but you probably want to follow it with a suitable
+filter.
+
+"""
+
+__all__ = ['InsertZero']
+__docformat__ = 'restructuredtext en'
+
+import numpy
+
+from pyctools.core.config import ConfigEnum
+from pyctools.core.base import Component
+from pyctools.core.types import pt_float
+
+class InsertZero(Component):
+    with_outframe_pool = True
+
+    def initialise(self):
+        self.first_field = True
+
+    def process_frame(self):
+        if self.first_field:
+            in_frame = self.input_buffer['input'].peek()
+        else:
+            in_frame = self.input_buffer['input'].get()
+        out_frame = self.outframe_pool['output'].get()
+        out_frame.initialise(in_frame)
+        in_data = in_frame.as_numpy()
+        audit = out_frame.metadata.get('audit')
+        audit += 'data = InsertZeroDeinterlace(data)\n'
+        out_frame.metadata.set('audit', audit)
+        out_frame.frame_no = in_frame.frame_no * 2
+        out_frame.data = numpy.zeros(in_data.shape, dtype=pt_float)
+        if self.first_field:
+            out_frame.data[0::2] = in_data[0::2]
+        else:
+            out_frame.data[1::2] = in_data[1::2]
+            out_frame.frame_no += 1
+        self.output(out_frame)
+        self.first_field = not self.first_field
