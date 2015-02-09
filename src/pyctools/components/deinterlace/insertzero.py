38cbe693abbcc945799aa8e660e955a46f19c3c3
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-02-09 10:23:31 +0000

    Added inverse option to zero-inserting deinterlacer
    This allows any sequence to be converted to interlace!

--- src/pyctools/components/deinterlace/insertzero.py
+++ src/pyctools/components/deinterlace/insertzero.py
@@ -27,6 +27,7 @@ filter.
 ============  ===  ====
 Config
 ============  ===  ====
+``inverse``   str  Can be set to ``off`` or ``on``.
 ``topfirst``  str  Top field first. Can be set to ``off`` or ``on``.
 ============  ===  ====
 
@@ -45,6 +46,7 @@ class InsertZero(Component):
     with_outframe_pool = True
 
     def initialise(self):
+        self.config['inverse'] = ConfigEnum(('off', 'on'))
         self.config['topfirst'] = ConfigEnum(('off', 'on'))
         self.config['topfirst'] = 'on'
         self.first_field = True
@@ -52,13 +54,19 @@ class InsertZero(Component):
     def process_frame(self):
         self.update_config()
         top_field_first = self.config['topfirst'] == 'on'
+        if self.config['inverse'] == 'on':
+            self.do_inverse(top_field_first)
+        else:
+            self.do_forward(top_field_first)
+
+    def do_forward(self, top_field_first):
         if self.first_field:
             in_frame = self.input_buffer['input'].peek()
+            self.in_data = in_frame.as_numpy()
         else:
             in_frame = self.input_buffer['input'].get()
         out_frame = self.outframe_pool['output'].get()
         out_frame.initialise(in_frame)
-        self.in_data = in_frame.as_numpy()
         audit = out_frame.metadata.get('audit')
         audit += 'data = InsertZeroDeinterlace(data)\n'
         out_frame.metadata.set('audit', audit)
@@ -70,5 +78,29 @@ class InsertZero(Component):
             out_frame.data[1::2] = self.in_data[1::2]
         if not self.first_field:
             out_frame.frame_no += 1
+        self.output(out_frame)
+        self.first_field = not self.first_field
+
+    def do_inverse(self, top_field_first):
+        in_frame = self.input_buffer['input'].get()
+        if self.first_field:
+            self.first_field_data = in_frame.as_numpy()
+            self.first_field = not self.first_field
+            return
+        out_frame = self.outframe_pool['output'].get()
+        out_frame.initialise(in_frame)
+        second_field_data = in_frame.as_numpy()
+        audit = out_frame.metadata.get('audit')
+        audit += 'data = InsertZeroReinterlace(data)\n'
+        out_frame.metadata.set('audit', audit)
+        out_frame.frame_no = in_frame.frame_no // 2
+        out_frame.data = numpy.empty(
+            second_field_data.shape, dtype=second_field_data.dtype)
+        if top_field_first:
+            out_frame.data[0::2] = self.first_field_data[0::2]
+            out_frame.data[1::2] = second_field_data[1::2]
+        else:
+            out_frame.data[1::2] = self.first_field_data[1::2]
+            out_frame.data[0::2] = second_field_data[0::2]
         self.output(out_frame)
         self.first_field = not self.first_field
