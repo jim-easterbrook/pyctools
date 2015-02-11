8466743b6f230765c1f33666416c68a70938c4a0
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-02-11 14:29:02 +0000

    Simple frame repeater component

---
+++ src/pyctools/components/framerepeat.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#  Pyctools - a picture processing algorithm development kit.
+#  http://github.com/jim-easterbrook/pyctools
+#  Copyright (C) 2015  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#
+#  This program is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see
+#  <http://www.gnu.org/licenses/>.
+
+"""Repeat each input frame a fixed number of times.
+
+============  ===  ====
+Config
+============  ===  ====
+``count``     int  Number of times to repeat each frame.
+============  ===  ====
+
+"""
+
+__all__ = ['FrameRepeat']
+__docformat__ = 'restructuredtext en'
+
+from pyctools.core.base import Component
+from pyctools.core.config import ConfigInt
+
+class FrameRepeat(Component):
+    with_outframe_pool = True
+
+    def initialise(self):
+        self.config['count'] = ConfigInt(min_value=1)
+        self.repeat_count = 0
+
+    def process_frame(self):
+        self.update_config()
+        count = self.config['count']
+        self.repeat_count += 1
+        if self.repeat_count >= count:
+            self.repeat_count = 0
+            in_frame = self.input_buffer['input'].get()
+        else:
+            in_frame = self.input_buffer['input'].peek()
+        out_frame = self.outframe_pool['output'].get()
+        out_frame.initialise(in_frame)
+        audit = out_frame.metadata.get('audit')
+        audit += 'data = FrameRepeat(data)\n'
+        audit += '    count = {}\n'.format(count)
+        out_frame.metadata.set('audit', audit)
+        self.output(out_frame)
