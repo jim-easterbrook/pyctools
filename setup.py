8de70ae19f0af5b2a68f0ab54f921cf6dcf7caf5
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2015-02-21 15:07:44 +0000

    Increment version number
    The last few commits have changed the developer API enough to merit a
    version increment.
    
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #  Pyctools - a picture processing algorithm development kit.
 #  http://github.com/jim-easterbrook/pyctools
-#  Copyright (C) 2014  Jim Easterbrook  jim@jim-easterbrook.me.uk
+#  Copyright (C) 2014-15  Pyctools contributors
 #
 #  This program is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -24,7 +24,7 @@ import os
 from setuptools import setup, Extension
 import sys
 
-version = '0.2.0'
+version = '0.3.0'
 
 # find packages
 packages = ['pyctools']
