23dc82b61fd257cff9550e821e5c04534ce5885a
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2016-01-25 10:14:49 +0000

    Added options to adjust image file writer
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- src/pyctools/components/io/imagefilewriter.py
+++ src/pyctools/components/io/imagefilewriter.py
@@ -1,12 +1,41 @@
-# This file is part of pyctools http://github.com/jim-easterbrook/pyctools
-# Copyright pyctools contributors
-# Released under the GNU GPL3 licence
+#  Pyctools - a picture processing algorithm development kit.
+#  http://github.com/jim-easterbrook/pyctools
+#  Copyright (C) 2014-16  Pyctools contributors
+#
+#  This program is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation, either version 3 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see
+#  <http://www.gnu.org/licenses/>.
 
 """Save still image file.
 
-This is a "pass through" component that can be inserted anywhere in a
-pipeline. It saves the first frame it receives to file using
+This component saves the first frame it receives to file using
 :py:meth:`PIL.Image.Image.save`.
+
+See the `PIL documentation
+<http://pillow.readthedocs.org/en/latest/handbook/image-file-formats.html>`_
+for details of the available formats and options.
+
+The ``options`` configuration should be a comma separated list of colon
+separated names and values, for example a JPEG file might have these
+options: ``'quality': 95, 'progressive': True``.
+
+===========  ===  ====
+Config
+===========  ===  ====
+``path``     str  Path name of file to be created.
+``format``   str  Over-ride the file format. This is normally derived from the ``path`` extension.
+``options``  str  A string of :py:meth:`PIL.Image.Image.save` options.
+===========  ===  ====
 
 """
 
@@ -15,7 +44,7 @@ import PIL.Image
 __all__ = ['ImageFileWriter']
 __docformat__ = 'restructuredtext en'
 
-from pyctools.core.config import ConfigPath
+from pyctools.core.config import ConfigPath, ConfigStr
 from pyctools.core.frame import Metadata
 from pyctools.core.base import Transformer
 
@@ -23,19 +52,27 @@ class ImageFileWriter(Transformer):
     def initialise(self):
         self.done = False
         self.config['path'] = ConfigPath(exists=False)
+        self.config['format'] = ConfigStr()
+        self.config['options'] = ConfigStr()
 
     def transform(self, in_frame, out_frame):
         if self.done:
             return True
         self.update_config()
         path = self.config['path']
+        fmt = self.config['format'] or None
+        options = eval('{' + self.config['options'] + '}')
         # save image
         image = in_frame.as_PIL()
-        image.save(path)
+        image.save(path, format=fmt, **options)
         # save metadata
         md = Metadata().copy(in_frame.metadata)
         audit = md.get('audit')
-        audit += '%s = data\n' % path
+        audit += '{} = data\n'.format(path)
+        if fmt:
+            audit += '    format: {}\n'.format(fmt)
+        if options:
+            audit += '    options: {}\n'.format(self.config['options'])
         md.set('audit', audit)
         md.to_file(path)
         self.done = True
