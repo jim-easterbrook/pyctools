89ee612c41a75d8113e79058dcdda1eeb7044088
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2016-03-05 12:45:44 +0000

    Change colour correction config
    It now uses 'hue' and 'saturation' for each, which is easier to
    understand.
    
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- src/pyctools/components/photo/colourcorrect.py
+++ src/pyctools/components/photo/colourcorrect.py
@@ -30,51 +30,90 @@ from pyctools.core.types import pt_float
 class ColourCorrect(Transformer):
     """Colour correction.
 
-    Apply a 3x3 matrix to RGB inputs to adjust the colours. The six
-    parameters set the matrix values. Those on the leading diagonal are
-    computed to preserve white balance and overall gain.
-    :py:class:`~pyctools.components.arithmetic.Arithmetic` is easily
-    used to adjust white balance and gain if needed.
+    Adjust hue and saturation of R, G & B channels separately.
 
     ===========  =====  ====
     Config
     ===========  =====  ====
-    ``RG``       float  Amount of ``G`` input in ``R`` output.
-    ``BG``       float  Amount of ``B`` input in ``R`` output.
-    ``GR``       float  Amount of ``R`` input in ``G`` output.
-    ``GB``       float  Amount of ``B`` input in ``G`` output.
-    ``BR``       float  Amount of ``R`` input in ``B`` output.
-    ``BG``       float  Amount of ``G`` input in ``B`` output.
+    ``R_hue``    float  Adjust hue of red primary.
+    ``R_sat``    float  Adjust saturation of red primary.
+    ``G_hue``    float  Adjust hue of green primary.
+    ``G_sat``    float  Adjust saturation of green primary.
+    ``B_hue``    float  Adjust hue of blue primary.
+    ``B_sat``    float  Adjust saturation of blue primary.
     ===========  =====  ====
 
     """
 
     def initialise(self):
-        self.config['RG'] = ConfigFloat(decimals=2)
-        self.config['RB'] = ConfigFloat(decimals=2)
-        self.config['GR'] = ConfigFloat(decimals=2)
-        self.config['GB'] = ConfigFloat(decimals=2)
-        self.config['BR'] = ConfigFloat(decimals=2)
-        self.config['BG'] = ConfigFloat(decimals=2)
+        self.config['R_hue'] = ConfigFloat(decimals=2)
+        self.config['R_sat'] = ConfigFloat(value=1.0, decimals=2)
+        self.config['G_hue'] = ConfigFloat(decimals=2)
+        self.config['G_sat'] = ConfigFloat(value=1.0, decimals=2)
+        self.config['B_hue'] = ConfigFloat(decimals=2)
+        self.config['B_sat'] = ConfigFloat(value=1.0, decimals=2)
 
     def transform(self, in_frame, out_frame):
+        # compute colour matrix
         self.update_config()
-        RG = self.config['RG']
-        RB = self.config['RB']
-        GR = self.config['GR']
-        GB = self.config['GB']
-        BR = self.config['BR']
-        BG = self.config['BG']
-        matrix = numpy.array(
-            [[1.0 - (RG + RB), RG,               RB],
-             [GR,              1.0 - (GR + GB),  GB],
-             [BR,              BG,               1.0 - (BR + BG)]],
-             dtype=pt_float)
+        R_hue = self.config['R_hue']
+        R_sat = self.config['R_sat']
+        G_hue = self.config['G_hue']
+        G_sat = self.config['G_sat']
+        B_hue = self.config['B_hue']
+        B_sat = self.config['B_sat']
+        # 'hue' matrix
+        hue_matrix = numpy.array(
+            [[1.0, 0.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 1.0]], dtype=pt_float)
+        hue_matrix += numpy.array(
+            [[ 0.0, 0.0, 0.0],
+             [ 0.5, 0.0, 0.0],
+             [-0.5, 0.0, 0.0]], dtype=pt_float) * pt_float(R_hue * R_sat)
+        hue_matrix += numpy.array(
+            [[0.0,  0.5, 0.0],
+             [0.0,  0.0, 0.0],
+             [0.0, -0.5, 0.0]], dtype=pt_float) * pt_float(G_hue * G_sat)
+        hue_matrix += numpy.array(
+            [[0.0, 0.0,  0.5],
+             [0.0, 0.0, -0.5],
+             [0.0, 0.0,  0.0]], dtype=pt_float) * pt_float(B_hue * B_sat)
+        # 'saturation' matrix - uses BT.709 RGB->Y as specified by sRGB
+        sat_matrix = numpy.array(
+            [[1.0, 0.0, 0.0],
+             [0.0, 1.0, 0.0],
+             [0.0, 0.0, 1.0]], dtype=pt_float)
+        sat_matrix += numpy.array(
+            [[0.2126 - 1.0, 0.0, 0.0],
+             [0.2126,       0.0, 0.0],
+             [0.2126,       0.0, 0.0]], dtype=pt_float) * pt_float(1.0 - R_sat)
+        sat_matrix += numpy.array(
+            [[0.0, 0.7152,       0.0],
+             [0.0, 0.7152 - 1.0, 0.0],
+             [0.0, 0.7152,       0.0]], dtype=pt_float) * pt_float(1.0 - G_sat)
+        sat_matrix += numpy.array(
+            [[0.0, 0.0, 0.0722],
+             [0.0, 0.0, 0.0722],
+             [0.0, 0.0, 0.0722 - 1.0]], dtype=pt_float) * pt_float(1.0 - B_sat)
+        matrix = numpy.dot(hue_matrix, sat_matrix)
+        # apply matrix
         in_data = in_frame.as_numpy(dtype=pt_float)
         out_frame.data = numpy.dot(in_data, matrix.T)
         # add audit
         audit = out_frame.metadata.get('audit')
-        audit += 'data = ColourCorrect(data, {}, {}, {}, {}, {}, {})\n'.format(
-            RG, RB, GR, GB, BR, BG)
+        audit += 'data = ColourCorrect(data)\n'
+        if R_hue != 0.0:
+            audit += '    R_hue: {}\n'.format(R_hue)
+        if R_sat != 1.0:
+            audit += '    R_sat: {}\n'.format(R_sat)
+        if G_hue != 0.0:
+            audit += '    G_hue: {}\n'.format(G_hue)
+        if G_sat != 1.0:
+            audit += '    G_sat: {}\n'.format(G_sat)
+        if B_hue != 0.0:
+            audit += '    B_hue: {}\n'.format(B_hue)
+        if B_sat != 1.0:
+            audit += '    B_sat: {}\n'.format(B_sat)
         out_frame.metadata.set('audit', audit)
         return True
