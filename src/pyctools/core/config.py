96300c7cf116de840697b6f707cb5b02202a78af
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2018-03-21 09:12:30 +0000

    Reinstated 'default' config values
    These were removed in commit 799a64a, but are useful in keeping
    redundant config out of saved editor scripts.
    
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- src/pyctools/core/config.py
+++ src/pyctools/core/config.py
@@ -94,8 +94,17 @@ class ConfigLeafNode(object):
         where it's relevant.
 
     """
+    def __new__(cls, value, default, **kwds):
+        self = super(ConfigLeafNode, cls).__new__(cls, value)
+        if default is None:
+            default = value
+        self.default = default
+        for key, value in kwds.items():
+            setattr(self, key, value)
+        return self
+
     def parser_add(self, parser, key):
-        parser.add_argument('--' + key, default=self, **self.parser_kw)
+        parser.add_argument('--' + key, default=self.default, **self.parser_kw)
 
     def get(self):
         """Return the config item's current value."""
@@ -115,18 +124,16 @@ class ConfigInt(ConfigLeafNode, int):
     """
     parser_kw = {'type' : int, 'metavar' : 'n'}
 
-    def __new__(cls, value=0, min_value=None, max_value=None):
+    def __new__(cls, value=0, default=None, min_value=None, max_value=None):
         if min_value is not None and value < min_value:
             value = min_value
         if max_value is not None and value > max_value:
             value = max_value
-        self = super(ConfigInt, cls).__new__(cls, value)
-        self.min_value = min_value
-        self.max_value = max_value
-        return self
+        return super(ConfigInt, cls).__new__(
+            cls, value, default, min_value=min_value, max_value=max_value)
 
     def __getnewargs__(self):
-        return int(self), self.min_value, self.max_value
+        return int(self), self.default, self.min_value, self.max_value
 
 
 class ConfigBool(ConfigInt):
@@ -135,14 +142,17 @@ class ConfigBool(ConfigInt):
     """
     parser_kw = {'type' : bool, 'metavar' : 'b'}
 
-    def __new__(cls, value=False, *args, **kwds):
+    def __new__(cls, value=False, default=None, **kwds):
         if value == 'on':
             value = True
         elif value == 'off':
             value = False
         else:
             value = bool(value)
-        return super(ConfigBool, cls).__new__(cls, value, *args, **kwds)
+        return super(ConfigBool, cls).__new__(cls, value, default)
+
+    def __getnewargs__(self):
+        return bool(self), self.default
 
     def __str__(self):
         return str(bool(self))
@@ -160,21 +170,18 @@ class ConfigFloat(ConfigLeafNode, float):
     """
     parser_kw = {'type' : float, 'metavar' : 'x'}
 
-    def __new__(cls, value=0.0, min_value=None, max_value=None,
+    def __new__(cls, value=0.0, default=None, min_value=None, max_value=None,
                 decimals=8, wrapping=False):
         if min_value is not None and value < min_value:
             value = min_value
         if max_value is not None and value > max_value:
             value = max_value
-        self = super(ConfigFloat, cls).__new__(cls, value)
-        self.min_value = min_value
-        self.max_value = max_value
-        self.decimals = decimals
-        self.wrapping = wrapping
-        return self
+        return super(ConfigFloat, cls).__new__(
+            cls, value, default, min_value=min_value, max_value=max_value,
+            decimals=decimals, wrapping=wrapping)
 
     def __getnewargs__(self):
-        return (float(self), self.min_value, self.max_value,
+        return (float(self), self.default, self.min_value, self.max_value,
                 self.decimals, self.wrapping)
 
 
@@ -184,9 +191,11 @@ class ConfigStr(ConfigLeafNode, six.text_type):
     """
     parser_kw = {'metavar' : 'str'}
 
-    def __new__(cls, value=''):
-        return super(ConfigStr, cls).__new__(cls, value)
-        return self
+    def __new__(cls, value='', default=None, **kwds):
+        return super(ConfigStr, cls).__new__(cls, value, default, **kwds)
+
+    def __getnewargs__(self):
+        return six.text_type(self), self.default
 
 
 class ConfigPath(ConfigStr):
@@ -195,7 +204,7 @@ class ConfigPath(ConfigStr):
     """
     parser_kw = {'metavar' : 'path'}
 
-    def __new__(cls, value='', exists=True):
+    def __new__(cls, value='', default=None, exists=True):
         if value:
             value = os.path.abspath(value)
             if exists:
@@ -206,12 +215,10 @@ class ConfigPath(ConfigStr):
                     value = ''
         else:
             value = ''
-        self = super(ConfigPath, cls).__new__(cls, value)
-        self.exists = exists
-        return self
+        return super(ConfigPath, cls).__new__(cls, value, default, exists=exists)
 
     def __getnewargs__(self):
-        return six.text_type(self), self.exists
+        return six.text_type(self), self.default, self.exists
 
 
 class ConfigEnum(ConfigStr):
@@ -227,7 +234,7 @@ class ConfigEnum(ConfigStr):
         setting new values.
 
     """
-    def __new__(cls, value=None, choices=[], extendable=False):
+    def __new__(cls, value=None, default=None, choices=[], extendable=False):
         choices = list(choices)
         if not value:
             value = choices[0]
@@ -236,16 +243,15 @@ class ConfigEnum(ConfigStr):
                 choices.append(value)
             else:
                 raise ValueError(str(value))
-        self = super(ConfigEnum, cls).__new__(cls, value)
-        self.choices = choices
-        self.extendable = extendable
+        self = super(ConfigEnum, cls).__new__(
+            cls, value, default, choices=choices, extendable=extendable)
         self.parser_kw = {'metavar' : 'str'}
         if not self.extendable:
             self.parser_kw['choices'] = self.choices
         return self
 
     def __getnewargs__(self):
-        return six.text_type(self), self.choices, self.extendable
+        return six.text_type(self), self.default, self.choices, self.extendable
 
 
 class ConfigParent(ConfigLeafNode, collections.OrderedDict):
@@ -254,10 +260,14 @@ class ConfigParent(ConfigLeafNode, collections.OrderedDict):
     Stores a set of child nodes in a :py:class:`dict`.
 
     """
+    def __new__(cls):
+        return super(ConfigParent, cls).__new__(cls, value={}, default=None)
+
     def __repr__(self):
         result = {}
         for key, value in self.items():
-            result[key] = value
+            if value != value.default:
+                result[key] = value
         return repr(result)
 
     def parser_add(self, parser, prefix=''):
@@ -276,7 +286,7 @@ class ConfigParent(ConfigLeafNode, collections.OrderedDict):
             self[key] = value
 
     def __setitem__(self, key, value):
-        if not isinstance(value, ConfigLeafNode):
+        if key in self:
             value = self[key].update(value)
         super(ConfigParent, self).__setitem__(key, value)
 
