4aa9de958dc0b2eb2670e31067836644564716a5
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2020-02-27 14:01:04 +0000

    Update video file reader
    Uses ffprobe to get image dimensions and sequence length. Added an
    option to trim length so repeats retain the same phase, e.g. a multiple
    of 4 frames for PAL experiments.
    
    Signed-off-by: Jim Easterbrook <jim@jim-easterbrook.me.uk>

--- src/pyctools/components/io/videofilereader.py
+++ src/pyctools/components/io/videofilereader.py
@@ -1,6 +1,6 @@
 #  Pyctools - a picture processing algorithm development kit.
 #  http://github.com/jim-easterbrook/pyctools
-#  Copyright (C) 2014-18  Pyctools contributors
+#  Copyright (C) 2014-20  Pyctools contributors
 #
 #  This program is free software: you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -22,6 +22,7 @@ __all__ = ['VideoFileReader']
 __docformat__ = 'restructuredtext en'
 
 from contextlib import contextmanager
+import json
 import logging
 import os
 import re
@@ -31,17 +32,22 @@ import sys
 
 import numpy
 
-from pyctools.core.config import ConfigBool, ConfigPath, ConfigEnum
+from pyctools.core.config import ConfigBool, ConfigEnum, ConfigInt, ConfigPath
 from pyctools.core.base import Component
 from pyctools.core.frame import Metadata
 from pyctools.core.types import pt_float
 
+
 class VideoFileReader(Component):
     """Read conventional video files (mp4, flv, AVI, etc.).
 
-    This component uses `FFmpeg <https://www.ffmpeg.org/>`_ to read
-    video from a wide variety of formats. Make sure you have installed
-    FFmpeg before attempting to use :py:class:`VideoFileReader`.
+    This component uses FFmpeg_ to read video from a wide variety of
+    formats. Make sure you have installed FFmpeg before attempting to
+    use :py:class:`VideoFileReader`.
+
+    The ``zperiod`` config item can be used to adjust the repeat period
+    so it is an integer multiple of a chosen number, e.g. 4 frames for a
+    PAL encoded sequence. It has no effect if ``looping`` is ``off``.
 
     ===========  ====  ====
     Config
@@ -50,19 +56,21 @@ class VideoFileReader(Component):
     ``looping``  str   Whether to play continuously. Can be ``'off'`` or ``'repeat'``.
     ``type``     str   Output data type. Can be ``'RGB'`` or ``'Y'``.
     ``16bit``    bool  Attempt to get greater precision than normal 8-bit range.
+    ``zperiod``  int   Adjust repeat period to an integer multiple of ``zperiod``.
     ===========  ====  ====
+
+    .. _FFmpeg: https://www.ffmpeg.org/
 
     """
 
     inputs = []
 
     def initialise(self):
-        self.frame_no = 0
-        self.generator = None
         self.config['path'] = ConfigPath()
         self.config['looping'] = ConfigEnum(choices=('off', 'repeat'))
         self.config['type'] = ConfigEnum(choices=('RGB', 'Y'))
         self.config['16bit'] = ConfigBool()
+        self.config['zperiod'] = ConfigInt(min_value=0)
 
     @contextmanager
     def subprocess(self, *arg, **kw):
@@ -79,72 +87,77 @@ class VideoFileReader(Component):
         """Generator process to read file"""
         self.update_config()
         path = self.config['path']
-        # open file to get dimensions
-        with self.subprocess(
-                ['ffmpeg', '-v', 'info', '-y', '-an', '-vn', '-i', path, '-'],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                stdin=open(os.devnull), universal_newlines=True) as sp:
-            for line in sp.stderr.read().splitlines():
-                match = re.search('(\d{2,})x(\d{2,})', line)
-                if match:
-                    xlen, ylen = map(int, match.groups())
-                    break
-            else:
-                self.logger.critical('Failed to open %s', path)
-                return
+        # probe file to get dimensions
+        cmd = ['ffprobe', '-hide_banner', '-loglevel', 'warning',
+               '-show_streams', '-select_streams', 'v:0',
+               '-print_format', 'json', path]
+        p = subprocess.Popen(
+            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = p.communicate()
+        if p.returncode:
+            error = error.decode('utf-8')
+            error = error.splitlines()[0]
+            self.logger.critical('ffprobe: %s', error)
+            return
+        output = output.decode('utf-8')
+        header = json.loads(output)['streams'][0]
+        xlen = header['width']
+        ylen = header['height']
+        zlen = header['duration_ts']
         # read file repeatedly to allow looping
+        frame_no = 0
         while True:
             # can change config once per outer loop
             self.update_config()
             bit16 = self.config['16bit']
             self.frame_type = self.config['type']
+            zperiod = self.config['zperiod']
+            looping = self.config['looping']
+            if frame_no > 0 and looping == 'off':
+                break
+            # update metadata
             self.metadata = Metadata().from_file(path)
             audit = self.metadata.get('audit')
             audit += 'data = %s\n' % path
             audit += '    type: %s, 16bit: %s\n' % (self.frame_type, bit16)
             self.metadata.set('audit', audit)
+            # set data parameters
             bps = {'RGB': 3, 'Y': 1}[self.frame_type]
             pix_fmt = {'RGB': ('rgb24', 'rgb48le'),
                        'Y':   ('gray', 'gray16le')}[self.frame_type][bit16]
-            bytes_per_line = xlen * ylen * bps
+            bytes_per_frame = xlen * ylen * bps
             if bit16:
-                bytes_per_line *= 2
+                bytes_per_frame *= 2
+            frames = zlen
+            if zlen > zperiod and zperiod > 1 and looping != 'off':
+                frames -= (frame_no + zlen) % zperiod
             # open file to read data
             with self.subprocess(
                     ['ffmpeg', '-v', 'warning', '-an', '-i', path,
                      '-f', 'image2pipe', '-pix_fmt', pix_fmt,
                      '-c:v', 'rawvideo', '-'],
                     stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                    stdin=open(os.devnull), bufsize=bytes_per_line) as sp:
-                while True:
+                    stdin=open(os.devnull), bufsize=bytes_per_frame) as sp:
+                for z in range(frames):
                     try:
-                        raw_data = sp.stdout.read(bytes_per_line)
+                        raw_data = sp.stdout.read(bytes_per_frame)
                     except Exception as ex:
                         self.logger.exception(ex)
                         return
-                    if len(raw_data) < bytes_per_line:
-                        break
                     if bit16:
                         image = numpy.fromstring(raw_data, dtype=numpy.uint16)
                         image = image.astype(pt_float) / pt_float(256.0)
                     else:
                         image = numpy.fromstring(raw_data, dtype=numpy.uint8)
-                    yield image.reshape((ylen, xlen, bps))
-            self.update_config()
-            if self.frame_no == 0 or self.config['looping'] == 'off':
-                return
+                    yield frame_no, image.reshape((ylen, xlen, bps))
+                    frame_no += 1
+
+    def on_start(self):
+        self.generator = self.file_reader()
 
     def process_frame(self):
         frame = self.outframe_pool['output'].get()
-        if not self.generator:
-            self.generator = self.file_reader()
-        try:
-            frame.data = next(self.generator)
-        except StopIteration:
-            self.stop()
-            return
+        frame.frame_no, frame.data = next(self.generator)
         frame.type = self.frame_type
-        frame.frame_no = self.frame_no
-        self.frame_no += 1
         frame.metadata.copy(self.metadata)
         self.send('output', frame)
