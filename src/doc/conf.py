fa814cbdf85f826da97f3948e3aad885039f0a1d
Jim Easterbrook <jim@jim-easterbrook.me.uk>
2023-08-30 15:56:45 +0100

    Tweak Sphinx configuration

--- src/doc/conf.py
+++ src/doc/conf.py
@@ -62,7 +62,8 @@ needs_sphinx = '2.0'
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary',
-	      'sphinx.ext.viewcode', 'sphinx.ext.intersphinx']
+              'sphinx.ext.viewcode', 'sphinx.ext.intersphinx',
+              'sphinx_rtd_theme']
 
 autoclass_content = 'class'
 autodoc_member_order = 'bysource'
@@ -254,11 +255,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-if on_rtd:
-    html_theme = 'default'
-else:
-    html_theme = 'sphinx_rtd_theme'
-    extensions.append('sphinx_rtd_theme')
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
